b9b513a4faffa69f32d887a9d403ee867cc9033e
dan <afy@dan-dev.local>
2023-11-05 16:46:06 +0700

    fix port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const fs = require('fs');
 // create our express app
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 // cors
 app.use(cors());
